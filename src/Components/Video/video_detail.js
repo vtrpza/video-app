5d7549a4fca2d43022163c41fad60aa38784b4dd
vtrpza <vhnpouza@gmail.com>
2018-06-14 20:46:19 -0300

    implemeting search bar and video details

---
+++ src/Components/Video/video_detail.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function VideoDetail({video}) {
+    
+    if (!video) {
+        return <div>Welcome, search above for a video you want to see.</div>
+    }
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default VideoDetail
