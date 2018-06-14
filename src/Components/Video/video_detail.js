61130682ca5a7472fe3ca596d940e88ebe032858
vtrpza <vhnpouza@gmail.com>
2018-06-14 20:53:19 -0300

    finished components and added bootstrap and styling

--- src/Components/Video/video_detail.js
+++ src/Components/Video/video_detail.js
@@ -6,11 +6,20 @@ function VideoDetail({video}) {
         return <div>Welcome, search above for a video you want to see.</div>
     }
     
+    const videoId = video.id.videoId;
+    const url = `https://www.youtube.com/embed/${videoId}`;
+
     return (
-        <div>
-            
+        <div className='video-detail col-md-8'>
+            <div className='embed-resposive embed-resposive-16y9'>
+                <iframe className='embed-resposive-item' src={url}></iframe>
+            </div>
+            <div className='details'>
+                <div>{video.snippet.title}</div>
+                <div>{video.snippet>description}</div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default VideoDetail
